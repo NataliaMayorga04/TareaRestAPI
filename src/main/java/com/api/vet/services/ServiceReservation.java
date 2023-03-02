31f4b2a4b1b0e28a101bea2e20fd1830127165b4
Lukas Nieto <lukasnieto117@gmail.com>
2023-03-02 00:04:59 -0500

    implementa clases servicio de reservas

--- src/main/java/com/api/vet/services/ServiceReservation.java
+++
@@ -1,9 +0,0 @@
-package com.api.vet.services;
-
-import com.api.vet.model.Reservation;
-
-public interface ServiceReservation {
-    Reservation saveReservation(Reservation reservation);
-
-    Reservation getReservation(Reservation Client);
-}
